4058e5d4f0952dae450586ca33dbf374364db286
sb763e <sb763e@att.com>
2024-12-04 22:31:10 -0500

    day01 - minor mods

--- day01/root.zig
+++ day01/root.zig
@@ -6,8 +6,8 @@ test "part1" {
 
     // create two lists
     var left_nums = std.ArrayList(u32).init(allocator);
-    var right_nums = std.ArrayList(u32).init(allocator);
     defer left_nums.deinit();
+    var right_nums = std.ArrayList(u32).init(allocator);
     defer right_nums.deinit();
 
     // get buffered reader
@@ -19,16 +19,14 @@ test "part1" {
     // fill left list and right list
     var buffer: [100]u8 = undefined;
     while (try reader.readUntilDelimiterOrEof(&buffer, '\n')) |line| {
-        var it = std.mem.splitSequence(u8, line, "  ");
+        var it = std.mem.tokenizeAny(u8, line, " ");
 
         // get left number
-        const sleft = std.mem.trim(u8, it.next().?, " ");
-        const ileft = try std.fmt.parseUnsigned(u32, sleft, 10);
+        const ileft = try std.fmt.parseUnsigned(u32, it.next().?, 10);
         try left_nums.append(ileft);
 
         // get right number
-        const sright = std.mem.trim(u8, it.next().?, " ");
-        const iright = try std.fmt.parseUnsigned(u32, sright, 10);
+        const iright = try std.fmt.parseUnsigned(u32, it.next().?, 10);
         try right_nums.append(iright);
         // std.debug.print("-{}-{}-\n", .{ ileft, iright });
     }
@@ -54,8 +52,8 @@ test "part2" {
 
     // create two lists
     var left_nums = std.ArrayList(u32).init(allocator);
-    var right_nums = std.ArrayList(u32).init(allocator);
     defer left_nums.deinit();
+    var right_nums = std.ArrayList(u32).init(allocator);
     defer right_nums.deinit();
 
     // get buffered reader
@@ -67,16 +65,14 @@ test "part2" {
     // fill left list and right list
     var buffer: [100]u8 = undefined;
     while (try reader.readUntilDelimiterOrEof(&buffer, '\n')) |line| {
-        var it = std.mem.splitSequence(u8, line, "  ");
+        var it = std.mem.tokenizeAny(u8, line, " ");
 
         // get left number
-        const sleft = std.mem.trim(u8, it.next().?, " ");
-        const ileft = try std.fmt.parseUnsigned(u32, sleft, 10);
+        const ileft = try std.fmt.parseUnsigned(u32, it.next().?, 10);
         try left_nums.append(ileft);
 
         // get right number
-        const sright = std.mem.trim(u8, it.next().?, " ");
-        const iright = try std.fmt.parseUnsigned(u32, sright, 10);
+        const iright = try std.fmt.parseUnsigned(u32, it.next().?, 10);
         try right_nums.append(iright);
         // std.debug.print("-{}-{}-\n", .{ ileft, iright });
     }
@@ -85,7 +81,7 @@ test "part2" {
     var mults: [99999]u8 = undefined;
     @memset(&mults, 0);
 
-    // iterate through right and sum
+    // iterate through right and increment counts
     for (right_nums.items) |num| {
         mults[num] += 1;
     }
