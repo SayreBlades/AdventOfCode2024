3469c55ad4628cd01ae84a9eae2bc1f7e4779ca0
Sayre <sayre.blades@gmail.com>
2024-12-27 13:40:43 -0500

    day05: binpacking baby!

--- day05/root.zig
+++ day05/root.zig
@@ -1,6 +1,13 @@
 const std = @import("std");
 const allocator = std.testing.allocator;
 const expect = std.testing.expect;
+const assert = std.debug.assert;
+const print = std.debug.print;
+
+fn is_on_robust(bitmap: *BitMap, x: u32, y: u32) bool {
+    return bitmap.is_on(x, y) catch false;
+}
+
 const trivial =
     \\47|53
     \\97|13
@@ -32,16 +39,48 @@ const trivial =
     \\97,13,75,29,47
 ;
 
-// TODO: use bitmask to optimize mem usage
-const BitMask = struct {
-    nums: [100][100]bool,
-    fn turn_on(self: *BitMask, x: u32, y: u32) void {
-        self.nums[x][y] = true;
+const BitMapError = error {
+    IndexTooBig
+};
+
+const BitMap = struct {
+    nums: u10000 = 0,
+    fn get_index(x: u32, y: u32) u14 {
+        return @intCast((x*100)+y);
     }
-    fn is_on(self: *BitMask, x: u32, y: u32) bool {
-        return self.nums[x][y];
+    fn turn_on(self: *BitMap, x: u32, y: u32) BitMapError!void {
+        const idx = get_index(x, y);
+        if(idx >= 10000) return BitMapError.IndexTooBig;
+        const bit:u10000 = 1;
+        self.nums = (bit << idx) | self.nums;
+    }
+    fn is_on(self: *BitMap, x: u32, y: u32) BitMapError!bool {
+        const idx = get_index(x, y);
+        if(idx >= 10000) return BitMapError.IndexTooBig;
+        const bit:u10000 = 1;
+        return ((bit << idx) & self.nums) > 0;
     }
 };
+
+test "BitMap" {
+    var bitmap = BitMap {};
+    // print("bitmap size: {}\n", .{@sizeOf(BitMap)});
+    try expect(try bitmap.is_on(0, 0) == false);
+    try expect(try bitmap.is_on(22, 99) == false);
+
+    if (bitmap.is_on(100, 100)) |_| {
+        try expect(false);
+    } else |err| switch (err) {
+        BitMapError.IndexTooBig =>  { }
+    }
+
+    try bitmap.turn_on(99, 99);
+    try expect(try bitmap.is_on(99, 99));
+
+    try bitmap.turn_on(0, 0);
+    try expect(try bitmap.is_on(0, 0));
+}
+
 
 fn part1(input: []const u8) !u32 {
     var lines_it = std.mem.splitSequence(u8, input, "\n\n");
@@ -49,13 +88,13 @@ fn part1(input: []const u8) !u32 {
 
     // std.debug.print("rules: {s}\n", .{rules});
     var rules_it = std.mem.splitSequence(u8, rules, "\n");
-    var bitmask: BitMask = undefined;
+    var bitmap = BitMap {};
     while (rules_it.next()) |rule| {
         // std.debug.print("rule: {s}\n", .{rule});
         var nums_it = std.mem.splitScalar(u8, rule, '|');
         const left = try std.fmt.parseUnsigned(u32, nums_it.next().?, 10);
         const right = try std.fmt.parseUnsigned(u32, nums_it.next().?, 10);
-        bitmask.turn_on(left, right);
+        try bitmap.turn_on(left, right);
     }
 
     const updates = lines_it.next() orelse unreachable;
@@ -74,7 +113,7 @@ fn part1(input: []const u8) !u32 {
             if (prev == null) {
                 continue;
             }
-            good = good and bitmask.is_on(prev.?, num);
+            good = good and try bitmap.is_on(prev.?, num);
             if (!good) break;
         }
         if (good) {
@@ -90,13 +129,13 @@ fn part2(input: []const u8) !u32 {
 
     // std.debug.print("rules: {s}\n", .{rules});
     var rules_it = std.mem.splitSequence(u8, rules, "\n");
-    var bitmask: BitMask = undefined;
+    var bitmap = BitMap{};
     while (rules_it.next()) |rule| {
         // std.debug.print("rule: {s}\n", .{rule});
         var nums_it = std.mem.splitScalar(u8, rule, '|');
         const left = try std.fmt.parseUnsigned(u32, nums_it.next().?, 10);
         const right = try std.fmt.parseUnsigned(u32, nums_it.next().?, 10);
-        bitmask.turn_on(left, right);
+        try bitmap.turn_on(left, right);
     }
 
     const updates = lines_it.next() orelse unreachable;
@@ -113,10 +152,14 @@ fn part2(input: []const u8) !u32 {
             try nums.append(num);
             defer prev = num;
             if (prev == null) continue;
-            good = good and bitmask.is_on(prev.?, num);
+            good = good and try bitmap.is_on(prev.?, num);
         }
         if (good) continue;
-        std.mem.sort(u32, nums.items, &bitmask, BitMask.is_on);
+
+
+        std.mem.sort(u32, nums.items, &bitmap, is_on_robust);
+
+        // std.mem.sort(u32, nums.items, &bitmap, BitMap.is_on);
         const middle = nums.items[(nums.items.len) / 2];
         sum += middle;
     }
@@ -134,10 +177,10 @@ test "part1" {
     var buffered = std.io.bufferedReader(file.reader());
     var reader = buffered.reader();
     var buffer: [20000]u8 = undefined;
+    // var buffer: [20000]u8 = [_]u8{0} ** 20000;
     const len = try reader.readAll(&buffer);
     // std.debug.print("{}\n", .{len});
     const sum = try part1(buffer[0..(len - 1)]);
-    // std.debug.print("{}\n", .{sum});
     try expect(sum == 5248);
 }
 
@@ -158,4 +201,14 @@ test "part2" {
     const sum = try part2(buffer[0..(len - 1)]);
     // std.debug.print("{}\n", .{sum});
     try expect(sum == 4507);
+}
+
+test "sizes" {
+    print("u1 size: {}\n", .{@sizeOf(u1)*8});
+    print("u7 size: {}\n", .{@sizeOf(u7)*8});
+    print("u8 size: {}\n", .{@sizeOf(u8)*8});
+    print("u9 size: {}\n", .{@sizeOf(u9)*8});
+    print("u100 size: {}\n", .{@sizeOf(u100)*8});
+    print("u128 size: {}\n", .{@sizeOf(u128)*8});
+    print("u129 size: {}\n", .{@sizeOf(u129)*8});
 }
